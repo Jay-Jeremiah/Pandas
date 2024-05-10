48de609d821a5c04fd222ec92a04e675bbeca96c
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-05-10 16:31:46 +0300

    replacing empty cells

---
+++ Data cleaning/fillna().py
@@ -0,0 +1,9 @@
+# fillna() attribute allows one to replace NUll values
+
+import pandas as pd
+
+df = pd.read_csv('C:\\Users\\Jeremiah\\Documents\\CSV_files\\data5.csv')
+
+df.fillna(130, inplace = True)
+
+print(df.to_string())
