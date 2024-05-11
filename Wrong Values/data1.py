e2a9e70734f6d055068d3bd798d17dd49494d0c1
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-05-11 14:49:54 +0300

    replacing values in pandas

--- Wrong Values/data1.py
+++ Wrong Values/data1.py
@@ -1,3 +1,5 @@
+# replacing values in a wrong format using loc[]
+
 import pandas as pd
 
 de = pd.read_csv('C:\\Users\\Jeremiah\\Documents\\CSV_files\\data8.csv')
@@ -7,4 +9,6 @@ de.loc[7, 'Duration'] = 45
 de.loc[14, 'Calories'] = 345.1
 
 de.loc[8, 'Date'] = '2024/12/21'
+
+
 print(de.to_string())
