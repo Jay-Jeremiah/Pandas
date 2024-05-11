4e395604d81cf4cd9e388f023eccd95d50431f5b
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-05-11 14:47:25 +0300

    replacing wrong values in pandas

---
+++ Wrong Values/data1.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+de = pd.read_csv('C:\\Users\\Jeremiah\\Documents\\CSV_files\\data8.csv')
+
+de.loc[7, 'Duration'] = 45
+
+de.loc[14, 'Calories'] = 345.1
+
+de.loc[8, 'Date'] = '2024/12/21'
+print(de.to_string())
